44f70b0eec11e7ebe6e7ccd860f09d082e9cd5e4
isabellalcvicente <isabellacadastros16@gmail.com>
2026-03-16 21:53:14 -0300

    testando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,17 @@ import { Extrato } from './pages/cliente/extrato/extrato';
 import { Registro } from './pages/auth/registro/registro';
 import { DashboardCliente } from './pages/cliente/dashboard-cliente/dashboard-cliente';
 import { ClienteMainPage } from './pages/cliente/cliente-main-page/cliente-main-page';
-
+import { SaqueCliente } from './pages/cliente/saque-cliente/saque-cliente';
+import { DepositoCliente } from './pages/cliente/deposito-cliente/deposito-cliente';
 export const routes: Routes = [
     {path: '', redirectTo: "login", pathMatch: 'full'},
 
     {path:'extrato', component: Extrato},
     {path: 'login', component: Login},
     {path: 'cliente-main-page', component: ClienteMainPage},
+    {path: 'deposito-cliente', component: DepositoCliente},
+    {path: 'saque-cliente', component: SaqueCliente}
+
 
 
 ];
