f8fed4cbc4de964a41cf8a8854d7f2ca24dc6f6d
isabellalcvicente <isabellacadastros16@gmail.com>
2026-03-19 22:24:07 -0300

    fix: alterações de layout

--- src/app/pages/cliente/extrato/extrato.ts
+++ src/app/pages/cliente/extrato/extrato.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-extrato',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './extrato.html',
   styleUrl: './extrato.css',
 })
 export class Extrato {
 
+  mensagem="";
+  dataInicio: any;
+  dataFim:any;
+
+  tirarExtrato() {
+    this.mensagem="Extrato realizado com sucesso"
+    return
+  }
 }
