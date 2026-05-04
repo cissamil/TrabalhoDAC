6ad595e1c147fb22f1b368cf6e8ab75012731d37
isabellalcvicente <isabellacadastros16@gmail.com>
2026-05-04 17:36:26 -0300

    feat: migração rest do perfil de adm, crud em relação a gerente

--- src/app/core/services/cliente-services/cliente-service.ts
+++ src/app/core/services/cliente-services/cliente-service.ts
@@ -4,6 +4,7 @@ import { Cliente, GerenteAdmin, PedidoAutoCadastro } from '../../models/entities
 import { CLIENTES_MOCK } from '../../mock/mock-data';
 import { PedidoAutoCadastroService } from '../pedido-autocadastro-services/pedido-autocadastro-service';
 import { ContaService } from '../conta-services/conta-service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const LS_CHAVE = 'clientes';
 
@@ -12,34 +13,54 @@ const LS_CHAVE = 'clientes';
 })
 export class ClienteService {
 
-  private clientesSubject: BehaviorSubject<Cliente[]>;
-  public clientes$: Observable<Cliente[]>
+  CLIENTE_URL="http://localhost:8080/clientes"
 
-  constructor(private pedidoAutoCadastroService: PedidoAutoCadastroService, private contaService: ContaService){
-    if(!localStorage[LS_CHAVE]){
-
-      localStorage[LS_CHAVE] = JSON.stringify(CLIENTES_MOCK);
-    }
-    
-    // localStorage[LS_CHAVE] = JSON.stringify(CLIENTES_MOCK);
-    const clientes: Cliente[]= localStorage[LS_CHAVE] ? JSON.parse(localStorage[LS_CHAVE]) : [];
-    this.clientesSubject = new BehaviorSubject<Cliente[]>(clientes);
-    this.clientes$ = this.clientesSubject.asObservable();
-
-    console.log(`MOCK de clientes inseridos, quantidade: ${clientes.length}`);
+  httpOptions={
+    headers: new HttpHeaders({
+      'Content-Type':'application/json'
+    })
   }
+
+  // private clientesSubject: BehaviorSubject<Cliente[]>;
+  // public clientes$: Observable<Cliente[]>
+
+  constructor(private httpClient: HttpClient){}
+  // constructor(private pedidoAutoCadastroService: PedidoAutoCadastroService, private contaService: ContaService){
+  //   if(!localStorage[LS_CHAVE]){
+
+  //     localStorage[LS_CHAVE] = JSON.stringify(CLIENTES_MOCK);
+  //   }
+
+    // localStorage[LS_CHAVE] = JSON.stringify(CLIENTES_MOCK);
+    //const clientes: Cliente[]= localStorage[LS_CHAVE] ? JSON.parse(localStorage[LS_CHAVE]) : [];
+    //this.clientesSubject = new BehaviorSubject<Cliente[]>(clientes);
+    //this.clientes$ = this.clientesSubject.asObservable();
+
+    //console.log(`MOCK de clientes inseridos, quantidade: ${clientes.length}`);
+  //}
 
   private atualizarDados(clientes: Cliente[]){
-    localStorage[LS_CHAVE] = JSON.stringify(clientes);
-    this.clientesSubject.next(clientes);
+    return this.httpClient.post<Cliente>(
+    this.CLIENTE_URL,
+    JSON.stringify(clientes),
+    this.httpOptions
+  );
   }
 
-  listarTodos() : Cliente[]{
-    return this.clientesSubject.getValue();
+  listarTodos() : Observable<Cliente[]>{
+    return this.httpClient.get<Cliente[]>(
+      this.CLIENTE_URL,
+      this.httpOptions
+    );
   }
 
-  inserir(cliente:Cliente): void{
+  inserir(cliente:Cliente): Observable<Cliente>{
 
+    return this.httpClient.post<Cliente>(
+      this.CLIENTE_URL,
+      JSON.stringify(cliente),
+      this.httpOptions
+    );
     // console.log(`
     //   Cliente a inserir:
     //   Nome: ${cliente.nome}
@@ -49,47 +70,47 @@ export class ClienteService {
     //   Endereço: ${cliente.endereco}
     // `);
 
-    try{
+    // try{
 
-      const clientes = this.listarTodos();
-      cliente.id = new Date().getTime();
-      clientes.push(cliente);
-      this.atualizarDados(clientes);
+    //   const clientes = this.listarTodos();
+    //   cliente.id = new Date().getTime();
+    //   clientes.push(cliente);
+    //   this.atualizarDados(clientes);
 
-      const gerente = this.contaService.buscarGerenteComMenosClientes();
+    //   const gerente = this.contaService.buscarGerenteComMenosClientes();
 
-      this.enviarSolicitacaoDeConta(cliente, gerente);
+    //   this.enviarSolicitacaoDeConta(cliente, gerente);
 
-      console.log("Gerente com menos clientes:", gerente.nome);
+    //   console.log("Gerente com menos clientes:", gerente.nome);
 
-    }catch(e){
+    // }catch(e){
 
-      console.error("Erro ao inserir usuário: ", e)
+    //   console.error("Erro ao inserir usuário: ", e)
 
-    }
+    // }
 
   }
 
-  private enviarSolicitacaoDeConta(cliente:Cliente, gerente: GerenteAdmin){
+  // private enviarSolicitacaoDeConta(cliente:Cliente, gerente: GerenteAdmin){
 
-    const pedidoCadastro: PedidoAutoCadastro ={
-      id: 0,
-      nomeCliente: cliente.nome,
-      nomeGerente: gerente.nome,
-      cpfCliente: cliente.cpf,
-      cpfGerente: gerente.cpf,
-      emailCliente: cliente.email,
-      salario: cliente.salario,
-      dataSolicitacao: new Date(),
-      status: 'PENDENTE'
-    }
+  //   const pedidoCadastro: PedidoAutoCadastro ={
+  //     id: 0,
+  //     nomeCliente: cliente.nome,
+  //     nomeGerente: gerente.nome,
+  //     cpfCliente: cliente.cpf,
+  //     cpfGerente: gerente.cpf,
+  //     emailCliente: cliente.email,
+  //     salario: cliente.salario,
+  //     dataSolicitacao: new Date(),
+  //     status: 'PENDENTE'
+  //   }
 
-    console.log("[SERVICE] Pedido a ser enviado:", pedidoCadastro);
+  //   console.log("[SERVICE] Pedido a ser enviado:", pedidoCadastro);
 
-    this.pedidoAutoCadastroService.inserir(pedidoCadastro);
-  }
+  //   this.pedidoAutoCadastroService.inserir(pedidoCadastro);
+  // }
 
-  atualizar(cliente: Cliente) : void{
+  atualizar(cliente: Cliente) : Observable<Cliente>{
     // console.log(`
     //   Dados a atualizar:
     //   Id: ${cliente.id}
@@ -99,45 +120,55 @@ export class ClienteService {
     //   Salario: ${cliente.salario}
     //   Endereço: ${cliente.endereco}
     // `);
-    const clientes = this.listarTodos();
+    //const clientes = this.listarTodos();
 
-    const index = clientes.findIndex(c => c.id === cliente.id);
-    if(index > -1){
-      clientes[index] = cliente;
-      this.atualizarDados(clientes);
-    }
+    // const index = clientes.findIndex(c => c.id === cliente.id);
+    // if(index > -1){
+    //   clientes[index] = cliente;
+    //   this.atualizarDados(clientes);
+    // }
+    return this.httpClient.put<Cliente>(
+      this.CLIENTE_URL + "/" + cliente.id,
+      JSON.stringify(cliente),
+      this.httpOptions
+    );
   }
 
-  buscarPorCPF(cpf: string){
-    const clientes = this.listarTodos();
+  // buscarPorCPF(cpf: string){
+  //   const clientes = this.listarTodos();
 
-    return clientes.find((cliente) => cliente.cpf === cpf);
+  //   return clientes.find((cliente) => cliente.cpf === cpf);
+  // }
+
+  remover(id: number) : Observable<Cliente>{
+    // const clientes = this.listarTodos()
+    //   .filter((c) => c.id !== id);
+
+    // this.atualizarDados(clientes);
+
+    return this.httpClient.delete<Cliente>(
+      this.CLIENTE_URL + "/" + id,
+      this.httpOptions
+    )
   }
 
-  remover(id: number) : void{
-    const clientes = this.listarTodos()
-      .filter((c) => c.id !== id);
+  // buscarClientePorEmail(email:string){
+  //   const clientes = this.listarTodos();
 
-    this.atualizarDados(clientes);
-  }
+  //   return clientes.find((cliente) => cliente.email === email);
+  // }
 
-  buscarClientePorEmail(email:string){
-    const clientes = this.listarTodos();
+  // buscarClientePorCPF(cpf:string){
+  //   const clientes = this.listarTodos();
 
-    return clientes.find((cliente) => cliente.email === email);
-  }
+  //   // console.log("Clientes: ", clientes);
+  //   return clientes.find((cliente) => cliente.cpf === cpf);
+  // }
 
-  buscarClientePorCPF(cpf:string){
-    const clientes = this.listarTodos();
-
-    // console.log("Clientes: ", clientes);
-    return clientes.find((cliente) => cliente.cpf === cpf);
-  }
-
-  buscarClientePorEmailESenha(email:string, senha:string){
-    const clientes = this.listarTodos();
-    return clientes.find((cliente) => cliente.email === email && cliente.senha === senha);
-  }
+  // buscarClientePorEmailESenha(email:string, senha:string){
+  //   const clientes = this.listarTodos();
+  //   return clientes.find((cliente) => cliente.email === email && cliente.senha === senha);
+  // }
 
 
 }
