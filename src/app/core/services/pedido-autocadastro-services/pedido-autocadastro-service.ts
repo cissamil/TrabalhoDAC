7134160225e5d5c345a75c41422cacd5873651d4
isabellalcvicente <isabellacadastros16@gmail.com>
2026-05-12 14:51:02 -0300

    testando

--- src/app/core/services/pedido-autocadastro-services/pedido-autocadastro-service.ts
+++ src/app/core/services/pedido-autocadastro-services/pedido-autocadastro-service.ts
@@ -2,44 +2,57 @@ import { Injectable } from '@angular/core';
 import { PedidoAutoCadastro } from '../../models/entities';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { CONTAS_MOCK, PEDIDOS_MOCK } from '../../mock/mock-data';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const LS_CHAVE = 'pedidos-conta';
+//const LS_CHAVE = 'pedidos-conta';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PedidoAutoCadastroService {
 
+  AUTOCADASTRO_URL ="http://localhost:8080"
 
-  private pedidosSubject: BehaviorSubject<PedidoAutoCadastro[]>;
-  public pedidos$: Observable<PedidoAutoCadastro[]>
-  
-  constructor(){
-    if(!localStorage[LS_CHAVE]){
+  httpOptions={
+    headers: new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+  }
 
-      localStorage[LS_CHAVE] = JSON.stringify(PEDIDOS_MOCK); 
+  //private pedidosSubject: BehaviorSubject<PedidoAutoCadastro[]>;
+  //public pedidos$: Observable<PedidoAutoCadastro[]>
+
+  constructor(private httpClient : HttpClient){
+   // if(!localStorage[LS_CHAVE]){
+     // localStorage[LS_CHAVE] = JSON.stringify(PEDIDOS_MOCK);
     }
-      
-    // localStorage[LS_CHAVE] = JSON.stringify(PEDIDOS_MOCK); 
 
-    const pedidos: PedidoAutoCadastro[]= localStorage[LS_CHAVE] ? JSON.parse(localStorage[LS_CHAVE]) : [];
-    this.pedidosSubject = new BehaviorSubject<PedidoAutoCadastro[]>(pedidos);
-    this.pedidos$ = this.pedidosSubject.asObservable();
+    // localStorage[LS_CHAVE] = JSON.stringify(PEDIDOS_MOCK);
 
-    console.log(`MOCK de pedidos inseridos, quantidade: ${pedidos.length}`);
+    //const pedidos: PedidoAutoCadastro[]= localStorage[LS_CHAVE] ? JSON.parse(localStorage[LS_CHAVE]) : [];
+    //this.pedidosSubject = new BehaviorSubject<PedidoAutoCadastro[]>(pedidos);
+    //this.pedidos$ = this.pedidosSubject.asObservable();
+
+    //console.log(`MOCK de pedidos inseridos, quantidade: ${pedidos.length}`);
+
+
+  // private atualizarDados(pedidos: PedidoAutoCadastro[]){
+  //   return this.httpClient.put<PedidoAutoCadastro>(
+  //     this.AUTOCADASTRO_URL,
+  //     JSON.stringify(pedidos),
+  //     this.httpOptions
+  //   );
+    //localStorage[LS_CHAVE] = JSON.stringify(pedidos);
+    //this.pedidosSubject.next(pedidos);
+  //}
+
+  listarTodos() : Observable<PedidoAutoCadastro[]>{
+    return this.httpClient.get<PedidoAutoCadastro[]>(
+      this.AUTOCADASTRO_URL,
+    );
   }
 
-  private atualizarDados(pedidos: PedidoAutoCadastro[]){
-    localStorage[LS_CHAVE] = JSON.stringify(pedidos);
-    this.pedidosSubject.next(pedidos);
-  }  
-
-  listarTodos() : PedidoAutoCadastro[]{
-
-    return this.pedidosSubject.getValue();
-  }
-
-  inserir(pedido: PedidoAutoCadastro): void{
+  inserir(pedido: PedidoAutoCadastro): Observable<PedidoAutoCadastro>{
 
     // console.log(`
     //   Pedido a inserir:
@@ -51,13 +64,18 @@ export class PedidoAutoCadastroService {
     //   Salario: ${pedido.salario}
     // `);
 
-    const pedidos = this.listarTodos();
-    pedido.id = new Date().getTime();
-    pedidos.push(pedido);
-    this.atualizarDados(pedidos);
+    //pedido.id = new Date().getTime();
+    //pedidos.push(pedido);
+    //this.atualizarDados(pedidos);
+
+    return this.httpClient.post<PedidoAutoCadastro>(
+      this.AUTOCADASTRO_URL,
+      JSON.stringify(pedido),
+      this.httpOptions
+    );
   }
 
-  atualizar(pedido: PedidoAutoCadastro) : void{
+  atualizar(pedido: PedidoAutoCadastro) : Observable<PedidoAutoCadastro>{
     // console.log(`
     //   Pedido a inserir:
     //   Cliente: ${pedido.nomeCliente},
@@ -67,15 +85,20 @@ export class PedidoAutoCadastroService {
     //   Data Solicitação: ${pedido.dataSolicitacao}
     //   Salario: ${pedido.salario}
     // `);
-    const pedidos = this.listarTodos();
-    
-    const index = pedidos.findIndex(p => p.id === pedido.id);
-    if(index > -1){
-      pedidos[index] = pedido;
-      this.atualizarDados(pedidos);
-    }
+    //const pedidos = this.listarTodos();
+
+    // const index = pedidos.findIndex(p => p.id === pedido.id);
+    // if(index > -1){
+    //   pedidos[index] = pedido;
+    //   this.atualizarDados(pedidos);
+    // }
+    return this.httpClient.put<PedidoAutoCadastro>(
+      this.AUTOCADASTRO_URL,
+      JSON.stringify(pedido),
+      this.httpOptions
+    )
   }
 
 
-  
+
 }
